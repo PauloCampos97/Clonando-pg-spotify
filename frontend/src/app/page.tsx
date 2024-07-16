67bf48ae0afba78d011c823524a8aa4a9078fca8
PauloCampos97 <paulocrz97@gmail.com>
2024-07-15 23:12:36 -0300

    Div a direita da pagina
    comecei a construir a div a direita da pagina criando criando os botões em setas

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { Button } from "@/components/button";
-import { House, Package, Search, Wifi } from "lucide-react";
+import { ChevronLeft, ChevronRight, House, Package, Search, Wifi } from "lucide-react";
 
 
 
@@ -10,8 +9,12 @@ export default function Home() {
 
   return (
     <div className="flex gap-2">
-      <div>
-        <div className="w-[420px] h-40 bg-zinc-900 m-2 p-6 flex flex-col justify-between rounded-lg">
+
+      {/*Div da parte esquerda da página */}
+      <div className="flex flex-col">
+
+        {/* Modal superior da parte esquerda  */}
+        <div className="w-[420px] h-40 bg-zinc-900 m-2 mr-0 p-6 flex flex-col justify-between rounded-lg">
           <div className="flex">
             <Wifi />
             <a href="#">Spotify</a>
@@ -28,8 +31,8 @@ export default function Home() {
           </div>
         </div>
 
-
-        <div className="w-[420px] h-[680px] bg-zinc-900 m-2 flex flex-col  rounded-lg">
+        {/* Modal inferior da parte esquerda */}
+        <div className="w-[420px] h-[680px] bg-zinc-900 m-2 mr-0 mt-0 flex flex-col  rounded-lg">
 
           <div className="flex gap-3 m-6">
             <Package />
@@ -45,16 +48,46 @@ export default function Home() {
           <div className="w-[400px] h-32 bg-zinc-800 rounded-lg mx-auto m-5">
             <h1 className="font-semibold text-sm mx-5 mt-4 mb-2">Que tal seguir um podcast novo?</h1>
             <p className="mx-5 text-sm font-normal">Avisaremos você sobre novos episódios.</p>
-            <button className="rounded-3xl px-4 py-1.5 m-5 font-bold flex bg-zinc-100 text-zinc-950 text-sm hover:scale-105">Explore podcasts</button>
+            <button className="rounded-3xl px-4 py-1.5 m-5 font-bold flex bg-zinc-100 text-zinc-950 text-sm hover:scale-105 border-white">Explore podcasts</button>
+          </div>
+
+          <div className="flex flex-wrap gap-5 font-extralight">
+            <a href="#">Legal</a>
+            <a href="#">Segurança e Centro de privacidade</a>
+            <a href="#">Políticas de privacidade</a>
+            <a href="#">Cookies</a>
+            <a href="#">Sobre anúncios</a>
+            <a href="#">Acessibilidade</a>
+            <button className="block shadow-shape rounded-3xl px-5 py-1 font-semibold hover:scale-105">Português do Brasil</button>
           </div>
         </div>
       </div>
 
 
 
+      {/* Parte direita da pagina */}
+      <div className="flex-1 rounded-lg bg-zinc-800 m-2 ml-0">
+        {/* div das setinhas */}
+        <div className="flex items-center space-x-3 h-16 bg-zinc-900 rounded-t-lg ">
+          <button className="ml-5 rounded-full bg-black/40 p-1">
+            <ChevronLeft />
+          </button>
+          <button className=" rounded-full bg-black/40 p-1">
+            <ChevronRight />
+          </button>
+        </div>
 
-      <div>
+        <div>
+          <div className="flex justify-between">
+            <h1 className="font-extrabold text-xl m-5">Artistas Populares</h1>
+            <a className="text-zinc-400 text-sm font-semibold m-5" href="#">Mostrar tudo</a>
+          </div>
 
+          <div>
+
+          </div>
+
+        </div>
       </div>
     </div>
   );
