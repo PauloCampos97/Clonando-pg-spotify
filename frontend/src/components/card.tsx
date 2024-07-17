1f11a4fe57ff59dfa7360ccbe65a93b1e9d62ed1
PauloCampos97 <paulocrz97@gmail.com>
2024-07-17 18:59:18 -0300

    Ajuste no button Play
    corrigindo posicionamento do button Play

--- frontend/src/components/card.tsx
+++ frontend/src/components/card.tsx
@@ -3,9 +3,14 @@ import { Play } from "lucide-react"
 
 export function ACards() {
     return (
-        <div className="max-w-60 min-h-52 p-4  rounded-lg hover:bg-white/5 transition-colors">
-            <a className=" rounded-lg " href="#">
+        <div className=" max-w-60 min-h-52 p-4  rounded-lg hover:bg-white/5 transition-colors">
+            <a className="relative min-w-36 rounded-lg " href="#">
                 <img className=" rounded-lg" src="album-soad.avif" alt="" />
+                <div className="absolute inset-0 flex -mb-24  items-center justify-end  rounded-full opacity-0 hover:opacity-100 transition-opacity">
+                    <button className=" p-3 rounded-full bg-green-400 text-black hover:scale-105">
+                        <Play />
+                    </button>
+                </div>
                 <h3 className="font-semibold m-1">
                     Sistem of a Down
                 </h3>
@@ -13,7 +18,7 @@ export function ACards() {
                     Artista
                 </p>
             </a>
-        </div>
+        </div >
 
     )
 }
@@ -21,10 +26,10 @@ export function ACards() {
 export function ACardsRounded() {
     return (
         <div className=" max-w-60 min-h-52 p-4  rounded-lg hover:bg-white/5 transition-colors">
-            <a className="relative rounded-lg " href="#">
+            <a className="relative min-w-36 rounded-lg " href="#">
                 <img className=" rounded-full" src="album-soad.avif" alt="" />
-                <div className="absolute inset-0  ml-36 rounded-full opacity-95 hover:opacity-100 transition-opacity">
-                    <button className=" p-3 rounded-full bg-green-400 text-black">
+                <div className="absolute inset-0 flex -mb-24 items-center justify-end  rounded-full opacity-0 hover:opacity-100 transition-opacity">
+                    <button className=" p-3 rounded-full bg-green-400 text-black hover:scale-105">
                         <Play />
                     </button>
                 </div>
