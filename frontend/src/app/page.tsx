d1300d82045ca6477d325320853dc9f1930465aa
PauloCampos97 <paulocrz97@gmail.com>
2024-07-18 19:39:29 -0300

    Ajuste na div esquerda com fixed
    Coloquei um fixed na div esquerda pra não ter scroll quando descer a div direita

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -13,59 +13,62 @@ export default function Home() {
 
       {/*Div da parte esquerda da página */}
       <div className="flex flex-col">
+        <div className="w-[420px] h-[850px] m-2 mr-0"></div>
+        <div className="fixed">
 
-        {/* Modal superior da parte esquerda  */}
-        <div className="w-[420px] h-40 bg-zinc-900 m-2 mr-0 p-6 flex flex-col justify-between rounded-lg">
-          <div className="flex">
-            <Wifi />
-            <a href="#">Spotify</a>
-          </div>
-
-          <div className="flex gap-5">
-            <House />
-            <a className="text-zinc-400 font-semibold" href="#">Início</a>
-          </div>
-
-          <div className="flex gap-5">
-            <Search />
-            <a className="text-zinc-400 font-semibold" href="#">Buscar</a>
-          </div>
-        </div>
-
-        {/* Modal inferior da parte esquerda */}
-        <div className="w-[420px] h-[680px] bg-zinc-900 m-2 mr-0 mt-0 flex flex-col justify-between rounded-lg">
-
-          <div className="flex flex-col ">
-            <div className="flex gap-3 m-6">
-              <Package />
-              <a className="text-zinc-400 text-base font-semibold hover:text-zinc-100 transition-all" href="#">Sua Biblioteca</a>
+          {/* Modal superior da parte esquerda  */}
+          <div className="w-[420px] h-40 bg-zinc-900 m-2 mr-0 p-6 flex flex-col justify-between rounded-lg">
+            <div className="flex">
+              <Wifi />
+              <a href="#">Spotify</a>
             </div>
 
-            <div className="w-[400px] h-32 bg-zinc-800 rounded-lg mx-auto">
-              <h1 className="font-semibold text-sm mx-5 mt-4 mb-2">Crie sua primeira playlist</h1>
-              <p className="mx-5 text-sm font-normal">É fácil, vamos te ajudar.</p>
-              <button className="rounded-3xl px-4 py-1.5 m-5 font-bold flex bg-zinc-100 text-zinc-950 text-sm hover:scale-105">Criar playlist</button>
+            <div className="flex gap-5">
+              <House />
+              <a className="text-zinc-400 font-semibold" href="#">Início</a>
             </div>
 
-            <div className="w-[400px] h-32 bg-zinc-800 rounded-lg mx-auto m-5">
-              <h1 className="font-semibold text-sm mx-5 mt-4 mb-2">Que tal seguir um podcast novo?</h1>
-              <p className="mx-5 text-sm font-normal">Avisaremos você sobre novos episódios.</p>
-              <button className="rounded-3xl px-4 py-1.5 m-5 font-bold flex bg-zinc-100 text-zinc-950 text-sm hover:scale-105 border-white">Explore podcasts</button>
+            <div className="flex gap-5">
+              <Search />
+              <a className="text-zinc-400 font-semibold" href="#">Buscar</a>
             </div>
           </div>
 
-          <div className="p-4 flex flex-col h-44 justify-between">
-            <div className="flex flex-wrap gap-3 font-extralight">
-              <a className="text-xs" href="#">Legal</a>
-              <a className="text-xs" href="#">Segurança e Centro de privacidade</a>
-              <a className="text-xs" href="#">Políticas de privacidade</a>
-              <a className="text-xs" href="#">Cookies</a>
-              <a className="text-xs" href="#">Sobre anúncios</a>
-              <a className="text-xs" href="#">Acessibilidade</a>
+          {/* Modal inferior da parte esquerda */}
+          <div className="w-[420px] h-[680px] bg-zinc-900 m-2 mr-0 mt-5 flex flex-col justify-between rounded-lg">
 
+            <div className="flex flex-col ">
+              <div className="flex gap-3 m-6">
+                <Package />
+                <a className="text-zinc-400 text-base font-semibold hover:text-zinc-100 transition-all" href="#">Sua Biblioteca</a>
+              </div>
+
+              <div className="w-[400px] h-32 bg-zinc-800 rounded-lg mx-auto">
+                <h1 className="font-semibold text-sm mx-5 mt-4 mb-2">Crie sua primeira playlist</h1>
+                <p className="mx-5 text-sm font-normal">É fácil, vamos te ajudar.</p>
+                <button className="rounded-3xl px-4 py-1.5 m-5 font-bold flex bg-zinc-100 text-zinc-950 text-sm hover:scale-105">Criar playlist</button>
+              </div>
+
+              <div className="w-[400px] h-32 bg-zinc-800 rounded-lg mx-auto m-5">
+                <h1 className="font-semibold text-sm mx-5 mt-4 mb-2">Que tal seguir um podcast novo?</h1>
+                <p className="mx-5 text-sm font-normal">Avisaremos você sobre novos episódios.</p>
+                <button className="rounded-3xl px-4 py-1.5 m-5 font-bold flex bg-zinc-100 text-zinc-950 text-sm hover:scale-105 border-white">Explore podcasts</button>
+              </div>
             </div>
-            <button className=" flex items-center gap-1 w-44 shadow-shape rounded-3xl px-3 py-1 text-xs font-semibold my-auto hover:scale-105">
-              <Globe />Português do Brasil</button>
+
+            <div className="p-4 flex flex-col h-44 justify-between">
+              <div className="flex flex-wrap gap-3 font-extralight">
+                <a className="text-xs" href="#">Legal</a>
+                <a className="text-xs" href="#">Segurança e Centro de privacidade</a>
+                <a className="text-xs" href="#">Políticas de privacidade</a>
+                <a className="text-xs" href="#">Cookies</a>
+                <a className="text-xs" href="#">Sobre anúncios</a>
+                <a className="text-xs" href="#">Acessibilidade</a>
+
+              </div>
+              <button className=" flex items-center gap-1 w-44 shadow-shape rounded-3xl px-3 py-1 text-xs font-semibold my-auto hover:scale-105">
+                <Globe />Português do Brasil</button>
+            </div>
           </div>
         </div>
       </div>
@@ -112,8 +115,61 @@ export default function Home() {
             <ACards />
           </div>
 
+          <div className="flex justify-between">
+            <h1 className="font-extrabold text-xl ml-5 mt-5">Estações de rádio populares</h1>
+            <a className="text-zinc-400 text-sm font-semibold mr-5 mt-5" href="#">Mostrar tudo</a>
+          </div>
+          <div className="flex  mb-5 p-2">
+            <ACards />
+            <ACards />
+            <ACards />
+            <ACards />
+            <ACards />
+            <ACards />
+          </div>
+
+          <div className="flex justify-between">
+            <h1 className="font-extrabold text-xl ml-5 mt-5">Paradas em destaque</h1>
+            <a className="text-zinc-400 text-sm font-semibold mr-5 mt-5" href="#">Mostrar tudo</a>
+          </div>
+          <div className="flex  mb-5 p-2">
+            <ACards />
+            <ACards />
+            <ACards />
+            <ACards />
+            <ACards />
+            <ACards />
+          </div>
+
+          <div className="flex justify-between">
+            <h1 className="font-extrabold text-xl ml-5 mt-5">Playlists do Spotify</h1>
+            <a className="text-zinc-400 text-sm font-semibold mr-5 mt-5" href="#">Mostrar tudo</a>
+          </div>
+          <div className="flex  mb-5 p-2">
+            <ACards />
+            <ACards />
+            <ACards />
+            <ACards />
+            <ACards />
+            <ACards />
+          </div>
+
+          <div className="flex justify-between">
+            <h1 className="font-extrabold text-xl ml-5 mt-5">Epsódios em alta</h1>
+            <a className="text-zinc-400 text-sm font-semibold mr-5 mt-5" href="#">Mostrar tudo</a>
+          </div>
+          <div className="flex  mb-5 p-2">
+            <ACards />
+            <ACards />
+            <ACards />
+            <ACards />
+            <ACards />
+            <ACards />
+          </div>
+
         </div>
       </div>
+
     </div >
   );
 }
